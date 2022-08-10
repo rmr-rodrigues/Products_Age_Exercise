68b6be5f8e062cd2ed50a74abbe1e6e6ebb78ff3
Ricardo Rodrigues <ricardorodrigues.mfr@gmail.com>
2022-08-10 15:09:54 +0100

    Parsing strings from main arguments into Option[LocalDateTime].

--- src/main/scala/com/exercise/older/products/Main.scala
+++ src/main/scala/com/exercise/older/products/Main.scala
@@ -1,5 +1,21 @@
 package com.exercise.older.products
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import scala.util.Try
+
 object Main extends App {
 
+  if(args.length == 2) {
+    val startDate = args(0)
+    val endDate = args(1)
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    val startDateTime = Try { LocalDateTime.parse(startDate, formatter) }.toOption
+    val endDatetime = Try { LocalDateTime.parse(endDate, formatter) }.toOption
+
+    println(s"Start Date: $startDateTime - End Date: $endDatetime")
+
+  }else {
+    println("There seems to be some problem with the arguments, try again!")
+  }
 }
